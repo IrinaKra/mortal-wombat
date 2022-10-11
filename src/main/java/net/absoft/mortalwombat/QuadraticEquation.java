c1474ededaf332dc77ff737aedec24d9f10450bb
Irina <doroshenko123@gmail.com>
2022-10-11 08:32:45 +0300

    class MathFunctionTest

--- src/main/java/net/absoft/mortalwombat/QuadraticEquation.java
+++ src/main/java/net/absoft/mortalwombat/QuadraticEquation.java
@@ -2,18 +2,23 @@ package net.absoft.mortalwombat;
 
 
 public class QuadraticEquation {
+
+    public String x1 ="";
+    public String x2 ="";
     public String solveEq(double a, double b, double c) {
         if(a == 0)
             throw new IllegalArgumentException();
-        double D = b * b - (4 * a * c);
-        if(D < 0) {
-            return "no roots";
-        } else if(D == 0) {
+        double d = b * b - (4 * a * c);
+        if(d < 0) {
+            return null;
+        } else if(d == 0) {
             return Double.toString(b / 2 / a * (-1));
+
         } else {
-            double x1 = (((-1) * b + Math.sqrt(D)) / (2 * a));
-            double x2 = (((-1) * b - Math.sqrt(D)) / (2 * a));
-            return Double.toString(x1) + " " + Double.toString(x2);
+             x1 = Double.toString(((-1) * b + Math.sqrt(d)) / (2 * a));
+             x2 = Double.toString(((-1) * b - Math.sqrt(d)) / (2 * a));
+            System.out.println(x1 + "    " + x2);
+            return "";
         }
     }
 }
